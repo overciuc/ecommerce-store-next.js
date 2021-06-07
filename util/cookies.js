0236f9094711c3ccb7a1c492b7c30a360ffc9f8b
Olga <o.verciuc@gmail.com>
2021-06-07 21:32:00 +0200

    Try to set the cart counter

--- util/cookies.js
+++ util/cookies.js
@@ -18,3 +18,51 @@ export function toggleAddedProductByProductId(productId) {
   cookies.set('addToBasket', newCookieValue);
   return newCookieValue;
 }
+export function parseCookieValue(value) {
+  try {
+    return JSON.parse(value);
+  } catch (err) {
+    return undefined;
+  }
+}
+/*
+import cookies from 'js-cookie';
+
+export function getQuantityCookieValue() {
+  const cookieValue = cookies.getJSON('quantity');
+  // Test if cookie value is an array
+  return Array.isArray(cookieValue)
+    ? // if it is, return the array value
+      cookieValue
+    : // if it's not, return an empty array
+      [];
+}
+
+export function addItemByProductId(id) {
+  // newCookieValue is the decoded version of whatever is inside the cookie; currently an array
+  const newCookieValue = [...getQuantityCookieValue()];
+
+  const quantityItemInCookie = newCookieValue.find(
+    (qantityItem) => qantityItem.id === id,
+  );
+
+  if (quantityItemInCookie) {
+    quantityItemInCookie.quantity = quantityItemInCookie.quantity + 1;
+  } else {
+    newCookieValue.push({
+      id: id,
+      quantity: 1,
+    });
+  }
+
+  cookies.set('quantity', newCookieValue);
+}
+
+export function parseCookieValue(value) {
+  try {
+    return JSON.parse(value);
+  } catch (err) {
+    return undefined;
+  }
+}
+*/
