2be809c25c9b7aecfe37059f50dd73d656507f6d
Olga <o.verciuc@gmail.com>
2021-06-17 11:01:47 +0200

    Create ley.config and Heroku deploy files

--- util/database.js
+++ util/database.js
@@ -1,10 +1,32 @@
 import camelcaseKeys from 'camelcase-keys';
 import dotenvSafe from 'dotenv-safe';
 import postgres from 'postgres';
+import setPostgresDefaultsOnHeroku from './setPostgresDefaultsOnHeroku';
 
+setPostgresDefaultsOnHeroku();
 dotenvSafe.config();
 
-const sql = postgres();
+// const sql = postgres();
+
+function connectOneTimeToDatabase() {
+  let sql;
+
+  if (process.env.NODE_ENV === 'production') {
+    sql = postgres();
+    // Heroku needs SSL connections but
+    // has an "unauthorized" certificate
+    // https://devcenter.heroku.com/changelog-items/852
+    sql = postgres({ ssl: { rejectUnauthorized: false } });
+  } else {
+    if (!globalThis.__postgresSqlClient) {
+      globalThis.__postgresSqlClient = postgres();
+    }
+    sql = globalThis.__postgresSqlClient;
+  }
+  return sql;
+}
+
+const sql = connectOneTimeToDatabase();
 
 export async function getProducts() {
   const products = await sql`SELECT * FROM products`;
---
+++ util/types.ts
@@ -0,0 +1,10 @@
+export type Product = {
+  id: number;
+  productName: string;
+  category: string;
+  productSlug: string;
+  productDescription: string;
+  productShortDescription: string;
+  productImage: string;
+  price: number;
+};
